631c828bb99044c71877439f191ced7fece4fff3
haybales <haleynicole126@gmail.com>
2022-05-06 14:40:57 -0600

    merged questions

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const promptProject = () => {
     {
       type: 'input',
       name: 'title',
-      message: 'What is the title of your project?'
+      message: 'What is the title of your project? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter the title of your project!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
