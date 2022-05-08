4b12977ef2f3251cc679f1c48576b429839266c1
haybales <haleynicole126@gmail.com>
2022-05-08 13:13:19 -0600

    updated template

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
 
-  
-  # dusty
-  
-  ## [Github](https://github.com/dustbunny)
-  
-      
+
+# kdsf
+
+## [GitHub](https://github.com/sdlkj)
+
+
+## About Me 
+
+we4orie
+    
+
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">lsdfkj</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                JavaScript
+              </h5>
+              <p>sfdlkj</p>
+              <a href="dsjklf" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">sdjkfjldksj</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                JavaScript, HTML, CSS, ES6, jQuery, Bootstrap, Node
+              </h5>
+              <p>dskjfklasd</p>
+              <a href="ksdjf" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          
+        </div>
+      </section>
+    
+
+    
--- app.js
+++ app.js
@@ -1,87 +1,90 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-inquirer
-const promptProject = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter the title of your project!');
-          return false;
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
         }
-      }
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of your project'
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Provide installation instructions'
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide usage information'
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'What are the guidelines for contributing?'
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Provide test instructions'
+    ]);
+};
+
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
     }
-  ]);
+    console.log(`
+    =================
+    Add a New Project
+    ================= 
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        });
 };
 
 
-const promptMore = () => {
-  console.log(`
-  =================
-Add more information about your project
-=================
-  `);
-  return inquirer.prompt([
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What did you build this project with? (Check all that apply)',
-      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ]);
-};
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
 
-promptProject()
-  .then(answers => console.log(answers))
-  .then(promptMore)
-  .then(moreAnswers => console.log(moreAnswers));
-// const pageHTML = generatePage(name, github);
+    fs.writeFile('./README.md', pageHTML, err => {
+      if (err) throw new Error(err);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
